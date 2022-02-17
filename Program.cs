0e308f91866d5b408f7f52a96cb31fe78171f93f
Rebecca Hjörvar <rebecca.hjorvarroutlook.com>
2022-02-17 13:36:40 +0100

    fixed console output

--- Program.cs
+++ Program.cs
@@ -3,15 +3,23 @@ var excelGenerator = new ExcelGenerator();
 string url = "https://www.nbi-handelsakademin.se/kontakt/";
 string nodeSelection = "//h4[(@class='light')]";
 
-/*
-Console.WriteLine($"Would you like to create an excel file containing the employees currently listed on - {url} - ?");
+
+Console.WriteLine("Would you like to create an excel file containing the employees currently listed on ");
+Console.WriteLine();
+Console.WriteLine($"- {url} - ?");
+Console.WriteLine();
 Console.WriteLine(" Y / N ");
+Console.WriteLine();
 
 string? input = Console.ReadLine();
+
 Console.Clear();
 
-Quote.PlainExit();
-*/
+if (input.ToLower() != "y")
+{
+    Quote.PlainExit();
+}
+
 var output = excelGenerator.CreateExcel(scraper.ScrapeData(url, nodeSelection), "mycsvexcelfile");
 Quote.CreatedFileExit(output);  
 
