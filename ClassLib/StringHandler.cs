2a7b852ab6d7c6115bdef72b75f8e88f67f8dbd7
Rebecca Hjörvar <rebecca.hjorvarroutlook.com>
2022-02-17 14:32:35 +0100

    Added summary to NameSubstrings()

--- ClassLib/StringHandler.cs
+++ ClassLib/StringHandler.cs
@@ -1,5 +1,9 @@
 public class StringHandler
 {
+    /// <summary>
+    /// Takes a full name and converts it to a firstname and the first letter of the last name.
+    /// </summary>
+    /// <param name="name">The name that should be handled</param>
     public string NameSubstrings(HtmlAgilityPack.HtmlNode name)
     {
             var firstName = name.InnerText.Split(' ').First();
